4845b17e8d6c7c3e8a5d60856a92c7827aceab66
Tom Soulanille <soul@prama.com>
2017-01-25 09:01:51 -0800

    t1: rising-edge trigger, show_counter()

--- t1.py
+++ t1.py
@@ -9,17 +9,22 @@ micropython.alloc_emergency_exception_buf(100)
 class T1:
     def __init__(self):
         self.led = m.Pin(0, mode=m.Pin.OUT)
-        self.sense = m.Pin(4, mode=m.Pin.IN, pull=m.Pin.PULL_UP)
+        self.sense = m.Pin(4, mode=m.Pin.IN)
+        self.cb = None
         self.counter = 0
 
     def start(self):
+        if self.cb:
+            self.cb.trigger(self.sense.IRQ_RISING)
+            return
+
         def cbf(pin):
-            was = m.disable_irq() # for demonstration
+            #was = m.disable_irq() # for demonstration
             self.counter += 1
             self.bip()
-            m.enable_irq(was)   # restore
+            #m.enable_irq(was)   # restore
         self.cb = self.sense.irq(handler=cbf,
-                                 trigger=self.sense.IRQ_FALLING)
+                                 trigger=self.sense.IRQ_RISING)
 
     def stop(self):
         #self.cb = self.sense.irq(handler=None,
@@ -33,3 +38,17 @@ class T1:
     def bip(self):
         self.led(0)
         self.led(1)
+
+    def show_counter(self):
+        try:
+            v = -1
+            while True:
+                while self.counter == v:
+                    m.idle()
+                v = self.counter
+                print(v, end='\r')
+                #t.sleep_ms(100)
+        except KeyboardInterrupt:
+            print()
+
+
