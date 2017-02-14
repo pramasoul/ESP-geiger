0369cf19240cf600b3c5acee5acdd760e3be96cb
Tom Soulanille <soul@prama.com>
2017-02-13 16:52:11 -0800

    ws1.py: serveOne(timeout)

--- ws1.py
+++ ws1.py
@@ -3,12 +3,12 @@ import socket
 
 class WS:
     def __init__(self):
-        self.header = b"""HTTP/1.0 200 OK
-Content-Type: text/html; charset=UTF-8
-Content-Encoding: UTF-8
-Content-Length: %d
-Connection: close
-
+        self.header = b"""HTTP/1.0 200 OK\r
+Content-Type: text/html; charset=UTF-8\r
+Content-Encoding: UTF-8\r
+Content-Length: %d\r
+Connection: close\r
+\r
 """
         self.prefix = b"""<!DOCTYPE html>
 <html>
@@ -23,7 +23,6 @@ Connection: close
         self.count = 0
 
     def start(self):
-        overhead = len(self.prefix) + len(self.suffix)
         addr = self.addr = socket.getaddrinfo('0.0.0.0', 8080)[0][-1]
         try:
             s = self.s = socket.socket()
@@ -31,38 +30,46 @@ Connection: close
             s.bind(addr)
             s.listen(2)
             print('listening on', addr)
-
-            while True:
-                try:
-                    cl, ca = s.accept()
-                    self.count += 1
-                    message = b'%d\n' % self.count
-                    print('client connected from', ca)
-                    cl_file = cl.makefile('rwb', 0)
-                    while True:
-                        line = cl_file.readline()
-                        print(line)
-                        if not line or line == b'\r\n':
-                            break
-                    #cl.send(self.prefix)
-                    #cl.send(str(self.count).encode('UTF8'))
-                    #cl.send(self.suffix)
-                    cl_file.write(self.header % (len(message) + overhead))
-                    cl_file.write(self.prefix)
-                    cl_file.write(message)
-                    cl_file.write(self.suffix)
-                except:
-                    raise
-                finally:
-                    cl_file.close()
-                    cl.close()
         except:
-            raise
-        finally:
             s.close()
+
+    def serveOne(self, timeout=0.1):
+        s = self.s
+        s.settimeout(timeout)
+        self.count += 1
+        message = b'%d\n' % self.count
+        n_bytes = len(self.prefix) \
+                  + len(self.suffix) \
+                  + len(message)
+        try:
+            cl, ca = s.accept()
+        except OSError:
+            n_bytes = 0
+        else:
+            print('client connected from', ca)
+            cl_file = cl.makefile('rwb', 0)
+            while True:
+                line = cl_file.readline()
+                print(line)
+                if not line or line == b'\r\n':
+                    break
+            #cl.send(self.prefix)
+            #cl.send(str(self.count).encode('UTF8'))
+            #cl.send(self.suffix)
+            cl_file.write(self.header % n_bytes)
+            cl_file.write(self.prefix)
+            cl_file.write(message)
+            cl_file.write(self.suffix)
+            cl_file.close()
+            cl.close()
+        finally:
+            return n_bytes
+
 
     def stop(self):
         self.s.close()
 
 ws = WS()
 ws.start()
+import pdb
+pdb.set_trace()
