d1ba3fbfbc4d3e22751ec344a6574057d005a6e8
Tom Soulanille <soul@prama.com>
2017-02-14 19:09:14 -0800

    ws1: refactor, not there yet

--- ws1.py
+++ ws1.py
@@ -1,4 +1,6 @@
 # web server - TAS
+import pdb
+
 import socket
 
 class WS:
@@ -10,17 +12,7 @@ Content-Length: %d\r
 Connection: close\r
 \r
 """
-        self.prefix = b"""<!DOCTYPE html>
-<html>
- <head>
-  <title>Trivial Web Server</title>
-  <link rel="icon" href="data:;base64,iVBORw0KGgo=">
- </head>
- <body>
-  <h1>Test</h1>
-"""
-        self.suffix = b"""</body></html>"""
-        self.count = 0
+        self.handlers = {}
 
     def start(self):
         addr = self.addr = socket.getaddrinfo('0.0.0.0', 8080)[0][-1]
@@ -33,43 +25,64 @@ Connection: close\r
         except:
             s.close()
 
-    def serveOne(self, timeout=0.1):
+    def handle_one(self, timeout=0.1):
         s = self.s
         s.settimeout(timeout)
-        self.count += 1
-        message = b'%d\n' % self.count
-        n_bytes = len(self.prefix) \
-                  + len(self.suffix) \
-                  + len(message)
         try:
             cl, ca = s.accept()
         except OSError:
-            n_bytes = 0
+            return 0
+        print('client connected from', ca)
+        #pdb.set_trace()
+        cl_file = cl.makefile('rwb', 0)
+        line = cl_file.readline().rstrip(b'\r\n')
+        print(line)
+        cmd, path, ver = line.split()
+        while True:
+            line = cl_file.readline()#.rstrip(b'\r\n')
+            print(line)
+            if not line or line == b'\r\n':
+                break
+        if cmd in self.handlers:
+            resp = self.handlers[cmd](path)
         else:
-            print('client connected from', ca)
-            cl_file = cl.makefile('rwb', 0)
-            while True:
-                line = cl_file.readline()
-                print(line)
-                if not line or line == b'\r\n':
-                    break
-            #cl.send(self.prefix)
-            #cl.send(str(self.count).encode('UTF8'))
-            #cl.send(self.suffix)
-            cl_file.write(self.header % n_bytes)
-            cl_file.write(self.prefix)
-            cl_file.write(message)
-            cl_file.write(self.suffix)
-            cl_file.close()
-            cl.close()
-        finally:
-            return n_bytes
+            resp = b'Huh?'
+        cl_file.write(self.header % len(resp))
+        cl_file.write(resp)
+        cl_file.close()
+        cl.close()
+        return len(resp)
 
 
     def stop(self):
         self.s.close()
 
+class GET_handler:
+    def __init__(self):
+        self.prefix = b"""<!DOCTYPE html>
+<html>
+ <head>
+  <title>Trivial Web Server</title>
+  <link rel="icon" href="data:;base64,iVBORw0KGgo=">
+ </head>
+ <body>
+  <h1>Test</h1>
+"""
+        self.suffix = b"""</body></html>"""
+        self.count = 0
+
+    def get(self, path):
+        self.count += 1
+        if path == b'/count':
+            return self.prefix + str(self.count).encode('UTF8') + self.suffix
+        return self.prefix + \
+            b'<pre>%s</pre>' % path + self.suffix
+
 ws = WS()
+gh = GET_handler()
+ws.handlers[b'GET'] = gh.get
 ws.start()
-import pdb
+while True:
+    ws.handle_one(1)
+ws.handle_one(10)
 pdb.set_trace()
