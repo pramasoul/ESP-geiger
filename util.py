f3e7015baf6fcf36c937789f9070ebab38542b3a
Tom Soulanille <soul@prama.com>
2017-05-02 12:42:51 -0700

    util: ifconfig()

--- util.py
+++ util.py
@@ -13,6 +13,11 @@ def df(path='/'):
     return _[1] * _[4]
 
 
+def ifconfig():
+    import network
+    w = network.WLAN()
+    return w.ifconfig()
+
 def pinServe():
     import machine
     pins = [machine.Pin(i, machine.Pin.IN) for i in (0, 2, 4, 5, 12, 13, 14, 15)]
